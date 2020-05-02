d4d9005cec083b242cd6d99ded78bbc85c51905d
Adam In Tae Gerard <adam.gerard@gmail.com>
2020-05-02 14:29:55 -0700

    Update example_class.cpp

--- examples/class/example_class.cpp
+++ examples/class/example_class.cpp
@@ -5,6 +5,7 @@
 using namespace ExampleNamespace; // now you don't have to prepend the namespace when calling methods, etc.
 using namespace std;
 
+// scope resolution operator to implement header class methods
 void example_class::example_method()
 {
     cout << "I'm a console out message" << endl;
