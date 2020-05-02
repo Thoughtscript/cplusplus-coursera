ff3aa2d147d1d3df0c928cee41c261de13a11527
Adam In Tae Gerard <adam.gerard@gmail.com>
2020-05-02 14:42:59 -0700

    Update example_class.h

--- examples/class/example_class.h
+++ examples/class/example_class.h
@@ -1,4 +1,7 @@
 // header file - think interface/abstract class skeleton - not likely to change and doesn't need to be recompiled
+#ifndef Example_Class_H // pragma guard - used at compile time to prevent redundant appending/prepending of compiled code
+#define Example_Class_H // if not already defined, add - otherwise it will be ignored
+
 namespace ExampleNamespace
 {
     class example_class
