9de9f5b8ef61638ca1182395d9f6fcc1885d0087
Adam In Tae Gerard <adam.gerard@gmail.com>
2020-05-02 14:32:20 -0700

    Update example_class.cpp

--- examples/class/example_class.cpp
+++ examples/class/example_class.cpp
@@ -1,6 +1,7 @@
 // example_class implementation
-#include "example_class.h" // header in local directory
 #include <iostream> // header in standard library
+#ifndef example_class.h // header in local directory with pragma guard
+#define example_class.h // if not already defined, add
 
 using namespace ExampleNamespace; // now you don't have to prepend the namespace when calling methods, etc.
 using namespace std;
