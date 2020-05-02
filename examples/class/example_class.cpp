84caf840d268ef558b6a7bab0ffd0d09bc23004c
Adam In Tae Gerard <adam.gerard@gmail.com>
2020-05-02 14:28:15 -0700

    Create example_class.cpp

---
+++ examples/class/example_class.cpp
@@ -0,0 +1,11 @@
+// example_class implementation
+#include "example_class.h" // header in local directory
+#include <iostream> // header in standard library
+
+using namespace ExampleNamespace; // now you don't have to prepend the namespace when calling methods, etc.
+using namespace std;
+
+void example_class::example_method()
+{
+    cout << "I'm a console out message" << endl;
+}
