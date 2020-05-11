b96ba7eaaaa99f19d1479abecf4f0f2c33b172ae
Adam In Tae Gerard <adam.gerard@gmail.com>
2020-05-10 18:34:48 -0700

    coursework

---
+++ examples/class_pointer/ExampleClass.h
@@ -0,0 +1,13 @@
+// header file - think interface/abstract class skeleton - not likely to change and doesn't need to be recompiled
+#ifndef Example_Class_H // pragma guard - used at compile time to prevent redundant appending/prepending of compiled code
+#define Example_Class_H // if not already defined, add - otherwise it will be ignored
+
+namespace ExampleNamespace {
+    class ExampleClass {
+        public:
+            int a, b;
+            void exampleMethod();
+    };
+}
+
+#endif
